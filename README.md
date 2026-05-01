e9595b9a0de61529fce4001acafb3da2e7a41206
GabrielCruz <Gabriel.cp.moraes@gmail.com>
2026-05-01 20:42:50 +0000

    Implementação das funções de adicionar e remover livros, com exemplo prático.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Projeto Integrador do segundo semestre da FATEC. Desenvolvimento de uma biblioteca pessoal para a aplicação dos conhecimentos adquiridos. 
 
 O primeiro commit foi realizado as configurações do typescript e a inicialização do repositório.
+O segundo commit trouxe a base de dados iniciais, como a estruturação dos dados e a apresentação da tela
+O terceiro commit foi necessário para o acerto das configurações
--- src/index.ts
+++ src/index.ts
@@ -44,3 +44,66 @@ function exibirBiblioteca(): void {
 }
 
 exibirBiblioteca();
+
+// Função para validar o índice do livro
+function indiceValido(indice: number): boolean {
+  return indice >= 0 && indice < titulos.length;
+}
+
+// Função para adicionar um novo livro à biblioteca
+function adicionarLivro(
+  titulo: string,
+  autor: string,
+  ano: number,
+  quantidadePaginas: number,
+): void {
+
+  // Valida se o título e o autor não estão vazios
+  if (titulo.trim() === '' || autor.trim() === '') {
+    console.log('Erro: título e autor não podem ser vazios.');
+    return;
+  }
+
+// Valida se o ano e a quantidade de páginas são maiores que zero
+  if (ano <= 0 || quantidadePaginas <= 0) {
+    console.log('Erro: ano e páginas devem ser maiores que zero.');
+    return;
+  }
+
+// Adiciona os dados do novo livro aos arrays correspondentes
+  titulos.push(titulo);
+  autores.push(autor);
+  anos.push(ano);
+  paginas.push(quantidadePaginas);
+  lido.push(false);
+  avaliacoes.push(0);
+
+  console.log(`Livro "${titulo}" adicionado com sucesso!`);
+}
+// Função para remover um livro da biblioteca com base no índice
+function removerLivro(indice: number): void {
+  if (!indiceValido(indice)) {
+    console.log('Erro: índice inválido.');
+    return;
+  }
+// Armazena o título do livro que será removido para exibir na mensagem de sucesso
+  const tituloRemovido: string = titulos[indice]!;
+
+  // Remove os dados do livro dos arrays correspondentes
+  titulos.splice(indice, 1);
+  autores.splice(indice, 1);
+  anos.splice(indice, 1);
+  paginas.splice(indice, 1);
+  lido.splice(indice, 1);
+  avaliacoes.splice(indice, 1);
+
+  // Exibe uma mensagem de sucesso indicando que o livro foi removido
+  console.log(`Livro "${tituloRemovido}" removido com sucesso!`);
+}
+// Testes: Adiciona novos livros, remove um livro e exibe a biblioteca atualizada
+adicionarLivro('Capitães da Areia', 'Jorge Amado', 1937, 280);
+adicionarLivro('A Revolução dos Bichos', 'George Orwell', 1945, 152);
+removerLivro(2);
+exibirBiblioteca()
+
+
